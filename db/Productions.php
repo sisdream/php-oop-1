88caf41cc57fe25908dd69c6ead20b3ef9c70419
sisdream <sissipascale2@gmail.com>
2024-03-04 15:26:25 +0100

    feat: procuctions

---
+++ db/Productions.php
@@ -0,0 +1,7 @@
+<?php
+$productions = [
+    $fight_club = new Production('film', 'Fight Club', 'inglese', 8),
+    $mare_fuori = new Production('serie', 'Mare Fuori', 'italiana'),
+    $gossip_girl = new Production('serie', 'Gossip Girl', 'inglese', 7),
+    $life_is_beautiful = new Production('film', 'La vita è bella', 'italiana', 9)
+];
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    require_once __DIR__ . '/db/Productions.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP</title>
+</head>
+<body>
+    <div class="container">
+        <table class="table mt-5">
+            <thead>
+                <tr>
+                    <th scope="col">Type</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Original language</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Best Seller</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
